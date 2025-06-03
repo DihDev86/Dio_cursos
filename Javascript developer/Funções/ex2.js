29d9b2253a93b04b615fb609be959897a983809c
Diêgo Ernandes <diegobass405@gmail.com>
2025-06-03 15:21:46 -0300

    Atualização

--- Javascript developer/Funções/ex2.js
+++ Javascript developer/Funções/ex2.js
@@ -1,5 +1,13 @@
+/*
+Código condições de pagamento:
+1- À vista débito,  recebe 10% de deconto;
+2- À vista no dinheito ou pix, recebe 15% de desconto;
+3- Em duas vezes, preço normal de etiqueta sem juros;
+4- Acima de duas vezes, preço normal de etiqueta mais 10% de juros. 
+*/
+
 const precoEtiqueta = 100;
-const formaDePagamento = 4;
+const formaDePagamento = 2;
 
 if(formaDePagamento === 1){
     console.log(precoEtiqueta - (precoEtiqueta * 0.1));
