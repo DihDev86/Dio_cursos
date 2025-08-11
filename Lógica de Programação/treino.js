91e330c41ccc6e5458ff10148c63f0713dffe623
Diêgo Ernandes <diegobass405@gmail.com>
2025-08-11 10:48:42 -0300

    Create treino.js

---
+++ Lógica de Programação/treino.js
@@ -0,0 +1,27 @@
+let nome = "Diego Ernandes";
+let idade = 38;
+let casado = true;
+let filhos = ["Maria Gabriela", "filho 2", "filho 3"];
+let tecnologias = ["JavaScript", "Cypress", "Postman"];
+let tecnologia = "Banco de Dados";
+tecnologias[3] = tecnologia;
+let novaLinguagem = "Java";
+tecnologias[0] = novaLinguagem;
+tecnologias[4] = "Github";
+let nome2 = "";
+let number = null;
+let nome1 = 
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof casado);
+console.log(filhos);
+console.log("Meu primeiro filho(a) foi " + filhos[0]);
+console.log(tecnologias[3]);
+console.log(tecnologias[0]);
+console.log(tecnologias);
+console.log(tecnologias.length);
+console.log(nome2);
+console.log(nome.length);
+console.log(nome1);
+console.log(number + idade);
